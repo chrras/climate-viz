f53642def86e7d325276bfc9f98589f7a8f81bde
Christoffer Rasmussen <christofferrasmussen.1987@gmail.com>
2015-04-17 16:55:06 -0400

    Develop CSV import

--- temperature_hour.js
+++ temperature_hour.js
@@ -1,143 +1,108 @@
-//random dataset
-
-
-// [temp, day, hour]
-var dataset = [[10.2, 1, 1], [14.2, 1, 2], [10.2, 1, 3], [14.2, 1, 4], [21.0, 2, 1], [3.2, 2, 2], [21.0, 2, 3], [3.2, 2, 4], [33.9, 3, 1], [24.0, 3, 2], [33.9, 3, 3], [24.0, 3, 4],
-[10.2, 4, 1], [14.2, 4, 2], [10.2, 4, 3], [14.2, 4, 4], [21.0, 5, 1], [3.2, 5, 2], [21.0, 5, 3], [3.2, 5, 4], [33.9, 6, 1], [24.0, 6, 2], [33.9, 6, 3], [24.0, 6, 4]]
-
-
-// Import data from CSV
-/*d3.csv('data-gitignore/temperatures_nyc.csv', function(error, dataset) {  // NEW
-  dataset.forEach(function(d) {                    // NEW
-    d.count = +d.count;                            // NEW
-  });*/
-
-
-
-/*var dataset = [];
-var day = 1;
-var hour = 0;
-
-for (var i = 0; i < 8760; i++) {
-    var temp = Math.random() * 35;
-    dataset.concat([temp, day, hour]);
-    
-    if (hour == 23) {
-        hour = 0;
-        day = day + 1;
-    } else {
-        hour = hour + 1;
-    }
-}*/
-
-var w = 400;
-var h = 400;
-var paddingTop = 0;
-var paddingBottom = 0;
-var paddingLeft = 20;
-var paddingRight = 10;
-var dotRadius = 5
-var dotSpacing = 0.5 // in percent of radius
-
-
-
-//Create SVG element
-var svg = d3.select("body")
-    .append("svg")
-    .attr("width", w)
-    .attr("height", h);
-
-var xScale = d3.scale.linear()
-    .domain([1, 6])
-    .range([dotRadius * (2 + dotSpacing), dotRadius * (2 + dotSpacing) * 6]);
-
-var yScale = d3.scale.linear()
-    .domain([1, 4])
-    .range([dotRadius * (2 + dotSpacing) * 4, dotRadius * (2 + dotSpacing)]);
-
-/*
-var xScale = d3.scale.linear()
-    .domain([
-        d3.min(dataset, function(d) { return d[1]; }),
-        d3.max(dataset, function(d) { return d[1]; })
-    ])
-    .range([0, w]);
-
-
-var yScale = d3.scale.linear()
-    .domain([0, d3.max(dataset, function(d) { return d[1]; })])
-    .range([h - padding, padding]);
-
-var rScale = d3.scale.linear()
-    .domain([0, d3.max(dataset, function(d) { return d[1]; })])
-    .range([2, 5]);
-*/
-
-//var formatAsPercentage = d3.format(".1%");
-
-
-
-//Define X axis
-var xAxis = d3.svg.axis()
-    .scale(xScale)
-    .orient("bottom")
-    .ticks(2);
-
-//Define Y axis
-var yAxis = d3.svg.axis()
-    .scale(yScale)
-    .orient("left")
-    .ticks(2);
-    //.tickFormat(formatAsPercentage);
-
-
-
-
-svg.selectAll("circle")
-    .data(dataset)
-    .enter()
-    .append("circle")
-    .attr("cx", function(d) {
-        //return xScale((d[1]-0.5) * dotRadius * 2.4);
-        return xScale(d[1]) + paddingLeft;
-    })
-    .attr("cy", function(d) {
-        //return (d[2]-0.5) * dotRadius * 2.4;
-        return yScale(d[2]);
-    })
-    //.attr("r", dotRadius)
-    .attr("r", function(d) {
-        return dotRadius;
-    })
-    .attr("fill", function(d) {
-        return "rgba(255, 0, 0, " + d[0] / 35 + ")";
+d3.csv("data-gitignore/temperatures_nyc.csv", function(error, dataset) {
+    dataset.forEach(function(d) {
+        d.tOutC = +d.tOutC;
+        d.day = +d.day;
+        d.hour = +d.hour;
     });
 
 
-/*svg.selectAll("circle")
-    .data(dataset)
-    .enter()
-    .append("circle")
-    .attr("cx", function(d) {
-        return xScale(d[0]);
-    })
-    .attr("cy", function(d) {
-        return yScale(d[1]);
-    });
-*/
+    //----------------------------------
+    // VARIABLES
+    //----------------------------------
 
 
+    var days = d3.max(dataset, function(d) { return d.day; })
+        - d3.min(dataset, function(d) { return d.day; })
+    var hours = d3.max(dataset, function(d) { return d.hour; })
+        - d3.min(dataset, function(d) { return d.hour; })
+
+    var tMin = d3.min(dataset, function(d) { return d.tOutC; })
+    var tMax = d3.max(dataset, function(d) { return d.tOutC; })
+
+    var w = 1500;
+    var h = 400;
+    var paddingTop = 0;
+    var paddingBottom = 0;
+    var paddingLeft = 25;
+    var paddingRight = 0;
+    var dotRadius = 2
+    var dotSpacing = 0 // in percent of radius
 
 
-//Create X axis
-svg.append("g")
-    .attr("class", "axis")
-    .attr("transform", "translate(" + paddingLeft + "," + xScale(4+1) + ")")
-    //.attr("transform", "translate(0," + xyScale(24+1) + ")")
-    .call(xAxis);
+    //----------------------------------
+    // FUNCTIONS
+    //----------------------------------
 
-//Create Y axis
-svg.append("g")
-    .attr("class", "axis")
-    .attr("transform", "translate(" + paddingLeft + ",0)")
-    .call(yAxis);
+
+    // Create SVG canvas
+    var svg = d3.select("body")
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h);
+
+
+    // Scales
+    var xScale = d3.scale.linear()
+        .domain([1, days])
+        .range([dotRadius * (2 + dotSpacing), dotRadius * (2 + dotSpacing) * days]);
+
+    var yScale = d3.scale.linear()
+        .domain([1, hours])
+        .range([dotRadius * (2 + dotSpacing) * hours, dotRadius * (2 + dotSpacing)]);
+
+    var colorScale = d3.scale.linear()
+        //.domain([tMin, tMax])
+        .domain([0, 30])
+        .range([0, 1]);
+
+
+    // Define X axis
+    var xAxis = d3.svg.axis()
+        .scale(xScale)
+        .orient("bottom")
+        .ticks(25);
+
+
+    // Define Y axis
+    var yAxis = d3.svg.axis()
+        .scale(yScale)
+        .orient("left")
+        .ticks(2);
+
+
+    //----------------------------------
+    // DRAW ELEMENTS ON SVG CANVAS
+    //----------------------------------
+
+
+    // Heatmap dots
+    svg.selectAll("circle")
+        .data(dataset)
+        .enter()
+        .append("circle")
+        .attr("cx", function(d) {
+            return xScale(d.day) + paddingLeft;
+        })
+        .attr("cy", function(d) {
+            return yScale(d.hour);
+        })
+        .attr("r", dotRadius)
+        .attr("fill", function(d) {
+            return "rgba(100, 200, 200, " + colorScale(d.tOutC) + ")";
+        });
+
+
+    //Create X axis
+    svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(" + paddingLeft + "," + xScale(24+1) + ")")
+        //.attr("transform", "translate(0," + xyScale(24+1) + ")")
+        .call(xAxis);
+
+    //Create Y axis
+    svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(" + paddingLeft + ",0)")
+        .call(yAxis);
+
+});
