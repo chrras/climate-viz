8e3d793beec7ad8a4868ff3af8a1487f34e07193
Christoffer Rasmussen <christofferrasmussen.1987@gmail.com>
2015-04-29 22:09:45 -0400

    Develop scalable month x-axis

--- heatmap_temperature.js
+++ heatmap_temperature.js
@@ -1,31 +1,56 @@
-d3.csv("data-gitignore/temperatures_nyc.csv", function(error, dataset) {
+parseDate = d3.time.format("%d-%m-%y").parse
+//parseTime = d3.time.format("%H/%M").parse
+//parseDate('2003/01/01')
+
+d3.csv("data-gitignore/temperatures_nyc_new.csv", function(error, dataset) {
     dataset.forEach(function(d) {
         d.tOutC = +d.tOutC;
         d.day = +d.day;
         d.hour = +d.hour;
+        d.date = parseDate(d.date);
+        //d.clock = parseTime(d.time);
+        
     });
 
+console.log(d3.min(dataset, function(d) { return d.date }));
+
+/*var dates = [
+    {"val":1,"key":"Jan"},
+    {"val":32,"key":"Feb"},
+    {"val":60,"key":"Mar"},
+    {"val":91,"key":"Apr"},
+    {"val":121,"key":"May"},
+    {"val":152,"key":"Jun"},
+    {"val":182,"key":"Jul"},
+    {"val":213,"key":"Aug"},
+    {"val":244,"key":"Sep"},
+    {"val":274,"key":"Oct"},
+    {"val":305,"key":"Nov"},
+    {"val":335,"key":"Dec"}
+    ];*/
 
     //----------------------------------
     // VARIABLES
     //----------------------------------
 
+    var days = 365;
+    var hours = 24;
 
-    var days = d3.max(dataset, function(d) { return d.day; })
-        - d3.min(dataset, function(d) { return d.day; });
+/*    var days = d3.max(dataset, function(d) { return d.date; })
+        - d3.min(dataset, function(d) { return d.date; });
     var hours = d3.max(dataset, function(d) { return d.hour; })
-        - d3.min(dataset, function(d) { return d.hour; });
+        - d3.min(dataset, function(d) { return d.hour; });*/
 
     var tMin = d3.min(dataset, function(d) { return d.tOutC; }),
         tMax = d3.max(dataset, function(d) { return d.tOutC; });
 
     var dotWidth = 1,       // zoom out = 1
-        dotHeight = 3,      // default 3 or 4
+        dotHeight = 4,      // default 3 or 4
         dotSpacing = 0.5;
 
-    var margin = {top: 0, right: 0, bottom: 0, left: 25},
+    var margin = {top: 0, right: 35, bottom: 40, left: 35},
         width = (dotWidth * 2 + dotSpacing) * days,
-        height = 200 - margin.top - margin.bottom;
+        height = (dotHeight * 2 + dotSpacing) * hours;//200 - margin.top - margin.bottom;
     
 
 
@@ -35,9 +60,21 @@ d3.csv("data-gitignore/temperatures_nyc.csv", function(error, dataset) {
     //----------------------------------
 
     // Scales
-    var xScale = d3.scale.linear()
-        .domain([1, days])
-        .range([dotWidth * 2 + dotSpacing, (dotWidth * 2 + dotSpacing) * days]);
+    // var xScale = d3.scale.linear()
+    //     .domain([1, days])
+    //     .range([dotWidth * 2 + dotSpacing, (dotWidth * 2 + dotSpacing) * days]);
+
+    // var xScale = d3.scale.ordinal()
+    //     .domain(dates.map(function (d) {return d.key; }))
+    //     .rangePoints([0,width]);
+
+    var xScale = d3.time.scale()
+        .domain([new Date(2015, 0, 1), new Date(2015, 11, 31)])
+        .range([0, width]);
+
+    console.log(xScale.domain())
+    console.log(xScale.range())
+    //console.log(d.hour);
 
     var yScale = d3.scale.linear()
         .domain([1, hours])
@@ -45,16 +82,24 @@ d3.csv("data-gitignore/temperatures_nyc.csv", function(error, dataset) {
 
     var colorScale = d3.scale.linear()
         .domain([tMin-5, (tMax-tMin)/2, tMax-5])
-        //.domain([tMin, 0, tMax])
         .range(["#4575b4","#ffffdf", "#d73027"]);
 
         
 
     // Define X axis
+    // var xAxis = d3.svg.axis()
+    //     .scale(xScale)
+    //     .orient("bottom");
+
     var xAxis = d3.svg.axis()
         .scale(xScale)
         .orient("bottom")
-        .ticks(10);
+        .ticks(d3.time.months)
+        .tickFormat(d3.time.format("%b"));
+        //.tickFormat(d3.time.format("%b%e" + "st"));
+
+
+    
 
     // Define Y axis
     var yAxis = d3.svg.axis()
@@ -62,28 +107,25 @@ d3.csv("data-gitignore/temperatures_nyc.csv", function(error, dataset) {
         .orient("left")
         .ticks(2);
 
-    // Zoom behaveuor
+    // Zoom behavior
     var zoom = d3.behavior.zoom()
         .scaleExtent([dotWidth, dotHeight])
         .x(xScale)
         .on("zoom", zoomHandler);
-    
+
     function zoomHandler() {
         var t = zoom.translate(),
             tx = t[0],
             ty = t[1];
 
-        tx = Math.min(tx, 0); // tx < 0
-        tx = Math.max(tx,  width - (dotWidth * 2 + dotSpacing) * days * d3.event.scale); //
+        tx = Math.min(tx, 0);
+        tx = Math.max(tx,  width - (dotWidth * 2 + dotSpacing) * days * d3.event.scale);
         zoom.translate([tx, ty]);
 
-        
-        //width - (dotWidth * d3.event.scale * 2 + dotSpacing)* days)
-        //(dotWidth * d3.event.scale + dotSpacing) * days - width
 
         svg.select(".x.axis").call(xAxis);
         svg.selectAll("ellipse")
-            .attr("cx", function(d) { return xScale(d.day); })
+            .attr("cx", function(d) { return xScale(d.date); })
             .attr("cy", function(d) { return yScale(d.hour); })
             .attr("rx", function(d) { return (dotWidth * d3.event.scale); });
     }
@@ -98,12 +140,15 @@ d3.csv("data-gitignore/temperatures_nyc.csv", function(error, dataset) {
         .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+
+
     // Clip path
     svg.append("clipPath")
         .attr("id", "clip")
         .append("rect")
-        .attr("width", width)
-        .attr("height", height);
+        .attr("width", width + 2 * dotHeight)
+        .attr("height", height + dotHeight)
+        .attr("transform", "translate(" + -dotHeight + ",0)");
 
 
     //----------------------------------
@@ -118,7 +163,7 @@ d3.csv("data-gitignore/temperatures_nyc.csv", function(error, dataset) {
         .data(dataset)
         .enter()
         .append("ellipse")
-        .attr("cx", function(d) { return xScale(d.day); })
+        .attr("cx", function(d) { return xScale(d.date); })
         .attr("cy", function(d) { return yScale(d.hour); })
         .attr("rx", dotWidth)
         .attr("ry", dotHeight)
@@ -128,12 +173,14 @@ d3.csv("data-gitignore/temperatures_nyc.csv", function(error, dataset) {
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + yScale(0) + ")")
-        .call(xAxis);
+        .call(xAxis)
 
     //Create Y axis
     svg.append("g")
         .attr("class", "y axis")
-        .attr("transform", "translate(" + xScale(0) + ",0)")
+        .attr("transform", "translate(" + -(2 * dotHeight + dotSpacing) + ",0)")
         .call(yAxis);
+
+    //console.log(d.date);
 
 });
